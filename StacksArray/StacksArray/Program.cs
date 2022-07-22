bd4ce94e3d43f856ffbdd85ed66adb86eb11eda6
kiaarablu <109739665+kiaarablu@users.noreply.github.com>
2022-07-22 04:19:32 -0700

    Add files via upload

---
+++ StacksArray/StacksArray/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stackarray kia = new Stackarray(8);
+            kia.push(1);
+            kia.push(2);
+            kia.push(3);
+            kia.push(4);
+            kia.push(5);
+            kia.push(6);
+            kia.push(7);
+            kia.push(8);
+            Console.WriteLine("----------------------------------------------------------------");
+            kia.pop();
+            kia.pop();
+            kia.pop();
+            kia.pop();
+            kia.pop();
+            kia.pop();
+            kia.pop();
+            kia.pop();
+            Console.ReadKey();
+
+        }
+    }
+
+
+}
---
+++ StacksArray/StacksArray/Stackarray.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksArray
+{
+     class Stackarray
+    {
+        public int[] stc;
+        public int top;
+        public int kol;
+        public  Stackarray (int sizearray)
+        {
+            this.stc = new int[sizearray];
+            top = -1;
+            kol = sizearray;
+        }
+
+        public void push(int a)
+        {
+            if (top == kol)
+            {
+                Console.WriteLine("stack overflow");
+                return;
+            }
+            else
+            {
+                top++;
+                stc[top] = a;
+            }
+        }
+        public void pop()
+        {
+            if (top == -1)
+            {
+                Console.WriteLine("stack underlow");
+                return;
+            }
+            else
+            {
+                Console.WriteLine(stc[top]);
+                top--;
+                return;
+                
+            }
+        }
+         
+
+    }
+    
+       
+    
+
+
+        
+       
+        
+    
+    
+        
+    
+    
+       
+    
+
+}
